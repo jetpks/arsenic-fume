5f4743af76a45de3affd3cd3d371514ab622f123
Eric Jacobs <eric@analog.sh>
2015-04-14 15:42:31 +0000

    Exhume whitespace.

--- fume.py
+++ fume.py
@@ -11,12 +11,9 @@ from Fume import dispatch
 # Config
 conf = ConfigParser.SafeConfigParser()
 conf.read('./fume.conf')
-
-
 dispatcher = dispatch.Dispatch(
         redis_use_socket=conf.get('redis', 'use_socket'),
         redis_socket=conf.get('redis', 'socket'))
-
 collector = collect.Collect(dispatcher, conf.get('zabbix', 'api'),
         conf.get('zabbix', 'user'), conf.get('zabbix', 'password'),
         hostgroups=[conf.get('zabbix', 'hostgroups')], interval=3)
